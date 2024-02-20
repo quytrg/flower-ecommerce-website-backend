aa38a8a7ca80dbf1093d0237369897992532e6cc
Truong Van Quy <truongvanquy2k3@gmail.com>
2024-02-20 13:29:44 +0700

    Split logic '[GET] /categories/product/:id' into Business Logic Layer and Data Access Layer (admin side)

--- app/controllers/admin/category.controller.js
+++ app/controllers/admin/category.controller.js
@@ -1,5 +1,6 @@
 const ApiError = require('../../middlewares/api-error.js')
 const CategoryService = require('../../services/admin/category.service.js')
+const ProductCategoryService = require('../../services/admin/product-category.service.js')
 
 // [GET] /categories/
 module.exports.find = async (req, res, next) => {
@@ -25,8 +26,17 @@ module.exports.find = async (req, res, next) => {
 module.exports.findCategoriesByProductId = async (req, res, next) => {
     try {
         const categoryService = new CategoryService()
+        const productCategoryService = new ProductCategoryService()
+
         const { id } = req.params
-        const categories = await categoryService.findCategoriesByProductId(id)
+        const categoryIds = await productCategoryService.findCategoryIdsByProductId(id)
+        const filter = {
+            _id: { $in: categoryIds },
+            deleted: false,
+            status: 'active'
+        }
+        const categories = await categoryService.find(filter)
+
         res.send(categories)    
     }
     catch (err) {
--- app/services/admin/category.service.js
+++ app/services/admin/category.service.js
@@ -1,5 +1,3 @@
-const ProductCategoryService = require('./product-category.service')
-
 class CategoryService {
     constructor() {
         this.Category = require('../../models/category.model')
@@ -7,24 +5,6 @@ class CategoryService {
 
     async find(filter, sort={ position: 'desc' }) {
         const categories = await this.Category.find(filter).sort(sort)
-        return categories
-    }
-
-    async findBySlug(slug) {
-        const category = await this.Category.findOne({ slug, deleted: false, status: 'active' })
-        return category
-    }
-
-    async findCategoriesByProductId(id) {
-        const productCategoryService = new ProductCategoryService()
-        const categoryIds = await productCategoryService.findCategoryIdsByProductId(id)
-
-        const filter = {
-            _id: { $in: categoryIds },
-            deleted: false,
-            status: 'active'
-        }
-        const categories = await this.Category.find(filter).select('title slug')
         return categories
     }
 }
