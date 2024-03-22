1ef3e7bc8f8811d23f559f99767125a56386510d
Truong Van Quy <truongvanquy2k3@gmail.com>
2024-03-22 13:50:39 +0700

    fix logic api '[GET] /categories/'

--- app/controllers/admin/category.controller.js
+++ app/controllers/admin/category.controller.js
@@ -2,22 +2,50 @@ const ApiError = require('../../middlewares/api-error.js')
 const CategoryService = require('../../services/admin/category.service.js')
 const ProductCategoryService = require('../../services/admin/product-category.service.js')
 
+// helpers
+const searchHelper = require('../../helpers/search.helper.js')
+const paginationHelper = require('../../helpers/pagination.helper.js')
+
+
 // [GET] /categories/
 module.exports.find = async (req, res, next) => {
     try {
         const categoryService = new CategoryService()
+
         const filter = {
             deleted: false,
         }
+
+        // filter by status
         if (req.query.status) {
             filter.status = req.query.status
         }
-        const categories = await categoryService.find(filter)
-        res.json(categories)
+
+        // seach
+        if (req.query.keyword) {
+            const searchObj = searchHelper(req.query)
+            filter.title = searchObj.regex
+        }
+
+        // pagination
+        const initPagination = {
+            currentPage: 1,
+            limit: 6
+        }
+        initPagination.totalRecords = await categoryService.count(filter)
+        const paginationObject = paginationHelper(req.query, initPagination)
+        
+        const categories = await categoryService.find(filter, paginationObject)
+        return res.status(200).json({
+            categories,
+            totalPages: paginationObject.totalPages,
+            limit: paginationObject.limit,
+            skip: paginationObject.skip
+        })
     }
     catch (err) {
         return next (
-            new ApiError(500, "An error occurred while retrieving the category")
+            new ApiError(500, "An error occurred while retrieving categories")
         )
     }
 }
--- app/services/admin/category.service.js
+++ app/services/admin/category.service.js
@@ -3,9 +3,17 @@ class CategoryService {
         this.Category = require('../../models/category.model')
     }
 
-    async find(filter, sort={ position: 'desc' }) {
-        const categories = await this.Category.find(filter).sort(sort)
+    async find(filter, pagination, sort={ position: 'desc' }) {
+        const categories = await this.Category.find(filter)
+                                        .limit(pagination.limit)
+                                        .skip(pagination.skip)
+                                        .sort(sort)
         return categories
+    }
+
+    async count(filter) {
+        const result = await this.Category.countDocuments(filter)
+        return result
     }
 }
 
