10910fa334e243618e568e117ee4e753394cd962
Truong Van Quy <truongvanquy2k3@gmail.com>
2024-03-26 11:14:55 +0700

    fix bug miss readAcounts permission middleware

--- app/middlewares/admin/permission.middleware.js
+++ app/middlewares/admin/permission.middleware.js
@@ -51,3 +51,16 @@ module.exports.deleteProducts = async (req, res, next) => {
         return next(new ApiError(500, 'An error occured while verifying permission'))
     }
 }
+
+module.exports.readAccounts = async (req, res, next) => {
+    try {
+        if (req.account.permissions.includes("read_accounts")) {
+            next();
+        } else {
+            return next(new ApiError(403, "Access denied"))
+        }
+    }
+    catch (err) {
+        return next(new ApiError(500, 'An error occured while verifying permission'))
+    }
+}
